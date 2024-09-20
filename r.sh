0b3952da2c5ef1df1dd57af6b68f679de182583b
阿笑 <huraboayard285@gmail.com>
2024-09-20 17:18:58 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,6 +1,8 @@
+sudo apt update
+sudo apt install rclone -y
 sudo apt-get install libjudy-dev -y
 git clone https://github.com/AxiaoXe/EVEV4.git
 chmod +x  EVEV4/*
-mv EVEV4/* /usr/bin
-rm -rf EVEV4
-/root/.config/rclone/rclone.conf
+find EVEV4/ -type f ! -name 'r.sh' -exec mv {} /usr/bin/ \;
+rm -rf /root/EVEV4
+
