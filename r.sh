d7a6c8f011ab3a2e6e16057f08aa1936699f22e2
阿笑 <huraboayard285@gmail.com>
2024-09-20 22:05:07 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -4,6 +4,6 @@ sudo apt install tmux -y
 sudo apt-get install libjudy-dev -y
 git clone https://github.com/AxiaoXe/EVEV4.git
 chmod +x  EVEV4/*
-find EVEV4/ -type f ! -name 'r.sh' -exec mv {} /usr/bin/ \;
+find EVEV4/ -type f ! \( -name 'r.sh' -o -name 'bs.sh' \) -exec mv {} /usr/bin/ \;
 rm -rf /root/EVEV4
 
